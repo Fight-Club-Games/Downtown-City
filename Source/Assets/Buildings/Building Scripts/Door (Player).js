93eb4a7af4a60eebd0c9034c556971e18c97a42e
Rooney Koplos <brick@fightclubgames.net>
2014-03-04 19:56:03 -0500

    Added new content
    Added improved door script and Mr Pigu

--- Source/Assets/Buildings/Building Scripts/Door (Player).js
+++ Source/Assets/Buildings/Building Scripts/Door (Player).js
@@ -2,14 +2,29 @@
 
 GameObject.Find("MessageGUI").guiText.enabled = false;
 
+var inTrigger = false;
+
 function OnTriggerEnter(trigger:Collider){
 	if(trigger.tag == "Door"){
-		GameObject.Find("MessageGUI").guiText.text = "Press 'E' to Enter";
+		inTrigger = true;
+		}
+}
+function Update(){
+	if(inTrigger){
 		GameObject.Find("MessageGUI").guiText.enabled = true;
+		if(Input.GetKey(KeyCode.E)){
+			GameObject.Find("MessageGUI").guiText.text = "This building is not available yet!";
+		}
+		else{
+			GameObject.Find("MessageGUI").guiText.text = "Press 'E' to Enter";
+		}
+	}
+	else{
+		GameObject.Find("MessageGUI").guiText.enabled = false;
 	}
 }
 function OnTriggerExit(trigger:Collider){
 	if(trigger.tag == "Door"){
-		GameObject.Find("MessageGUI").guiText.enabled = false;
+		inTrigger = false;
 	}
 }
