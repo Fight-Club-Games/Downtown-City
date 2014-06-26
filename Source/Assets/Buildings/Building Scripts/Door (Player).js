d7114d6187c497df4d066decb61a2a4eba5dde4e
Rooney Koplos <brick@fightclubgames.net>
2014-06-26 11:22:53 -0400

    Added animation and company interior
    Mr. Pigu is now animated and you can now view the company interior

--- Source/Assets/Buildings/Building Scripts/Door (Player).js
+++ Source/Assets/Buildings/Building Scripts/Door (Player).js
@@ -2,18 +2,44 @@
 
 GameObject.Find("MessageGUI").guiText.enabled = false;
 
+
+
 var inTrigger = false;
+var inOffice = false;
+function Awake(){
+if(PlayerPrefs.GetInt("FirstSpawn")){
+transform.position = Vector3( PlayerPrefs.GetFloat("PlayerX"), PlayerPrefs.GetFloat("PlayerY"), PlayerPrefs.GetFloat("PlayerZ") );
+}
+}
 
 function OnTriggerEnter(trigger:Collider){
 	if(trigger.tag == "Door"){
 		inTrigger = true;
 		}
+		else if(trigger.tag == "officeinterior"){
+			inOffice = true;
+		}
+		
 }
 function Update(){
 	if(inTrigger){
 		GameObject.Find("MessageGUI").guiText.enabled = true;
 		if(Input.GetKey(KeyCode.E)){
 			GameObject.Find("MessageGUI").guiText.text = "This building is not available yet!";
+		}
+		else{
+			GameObject.Find("MessageGUI").guiText.text = "Press 'E' to Enter";
+		}
+	}
+	if(inOffice){
+		GameObject.Find("MessageGUI").guiText.enabled = true;
+		if(Input.GetKey(KeyCode.E)){
+		PlayerPrefs.SetFloat("PlayerX", transform.position.x);
+PlayerPrefs.SetFloat("PlayerY", transform.position.y);
+PlayerPrefs.SetFloat("PlayerZ", transform.position.z);
+PlayerPrefs.SetInt("FirstSpawn",1);
+PlayerPrefs.Save();
+			Application.LoadLevel("officeinterior");
 		}
 		else{
 			GameObject.Find("MessageGUI").guiText.text = "Press 'E' to Enter";
@@ -27,4 +53,7 @@ function OnTriggerExit(trigger:Collider){
 	if(trigger.tag == "Door"){
 		inTrigger = false;
 	}
+	else if(trigger.tag == "officeinterior"){
+			inOffice = false;
+		}
 }
---
+++ Source/Assets/CompanyInterior/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour {
+
+	public Texture2D exit;
+
+	private void OnGUI () {
+		if (GUI.Button (new Rect (500, 120, 122, 120), exit)) {
+			Application.LoadLevel ("DowntownCity");
+				}
+	}
+}
